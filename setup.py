e62a5082a6ac32e52323eeef3df7383619beef6b
zeeshanonly <zeeshan.arshad891@gmail.com>
2024-11-10 23:21:16 +0100

    Here comes the installable file. Brace yourself

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name="dsss_homework_2_Zeeshan_Arshad",
+    version="0.1",
+    author="Zeeshan",
+    author_email="zeeshan.arshad@fau.de",
+    packages=find_packages(),
+    install_requires=["random", "unittest", "math_quiz"],
+)
